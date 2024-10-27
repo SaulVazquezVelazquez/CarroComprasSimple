8835a410daef47c8163e9c45b69fac150ff8b4f1
SaulVazquezVelazquez <saul.vazquezv@gmail.com>
2024-10-27 17:41:00 -0600

    Complementos centrar contenido / dividir nav / estilizar contenido - Success 2024-10-27 17:40

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 import Productos from './components/Productos'
-import ProdcutosTenis from './components/ProdcutosTenis.js'
+import Layout from './components/Layout'
+import Navbar from './components/Navbar'
+import Title from './components/Title'
 
 class App extends Component {
 
@@ -9,27 +11,24 @@ class App extends Component {
       {nombre : 'Tomate' , price : 1500 , img : '/productos/tomate.jpg' }, 
       {nombre : 'Arbejas' , price : 2500 , img : '/productos/arbejas.jpg' }, 
       {nombre : 'Lechuga' , price : 500 , img : '/productos/lechuga.jpg' }, 
-    ],
-    productosTenis : [
       {nombre : 'Nike' , price : 4500 , img : '/productos/nike.jpg'},
-      {nombre : 'Adidas' , price : 3500 , img : '/productos/adidas.jpg'},
+      {nombre : 'Adidas' , price : 3500 , img : '/productos/nike.jpg'},
       {nombre : 'Reebok' , price : 2500 , img : '/productos/reebok.jpg'}
-    ]  
+    ]
   }
 
   render(){
     return(
       <div>
-          <Productos 
-          agregarAlCarro={() => console.log("No hace nada") }
-          productos={this.state.productos}
-          />   
-
-          <ProdcutosTenis 
-          agregarAlCarro = {() => console.log("no hace nada tenis")}
-          productosTenis = {this.state.productosTenis}
-          />
-
+          <Navbar />
+          <Layout>
+            <Title />
+            <Productos 
+            agregarAlCarro={() => console.log("No hace nada") }
+            productos={this.state.productos}
+            />   
+          </Layout>
+          
       </div>
     )
   }
